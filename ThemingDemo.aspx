6600b21019186425d154ea29b8fa45747fd23ad4
Akansha-AP <42272867+Akansha-AP@users.noreply.github.com>
2020-12-04 23:37:56 +0530

    Add files via upload

---
+++ ThemingDemo.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ThemingDemo.aspx.cs" Inherits="Lab5Q3.ThemingDemo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Panel ID="pnl" runat="server" Height="300px">
+            <asp:Label ID="Label1" runat="server" Text="Select a theme:"></asp:Label>
+            <asp:DropDownList ID="DropDownList1" AutoPostBack="true" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" runat="server">
+            </asp:DropDownList>
+                </asp:Panel>
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ ThemingDemo.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab5Q3
+{
+    public partial class ThemingDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                string[] themes = { "Summer", "Monsoon" };
+                DropDownList1.DataSource = themes;
+                DropDownList1.DataBind();
+            }
+            if (Session["Theme"] != null)
+            {
+                DropDownList1.Items.FindByText(((ListItem)Session["Theme"]).Text).Selected = true;
+
+            }
+        }
+
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            if (Session["Theme"] != null)
+            {
+                Page.Theme =((ListItem)Session["Theme"]).Text;
+                /*if (Session["Theme"].ToString() == "Summer")
+                {
+                    pageBody.Attributes.Add("bgcolor", "#B7FFFF");
+                    Label1.Text = "Summer";
+                }
+                else if (Session["Theme"].ToString() == "Monsoon")
+                {
+                    pageBody.Attributes.Add("bgcolor", "#D8D8D8");
+                    Label1.Text = "Monsoon";
+                }*/
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Session["Theme"] = DropDownList1.SelectedItem;
+            Response.Redirect(Request.FilePath);
+        }
+    }
+}
