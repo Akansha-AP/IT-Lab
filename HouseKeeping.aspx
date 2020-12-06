991499bc514df3f1ae36b9408bf099ea498c8309
Akansha-AP <42272867+Akansha-AP@users.noreply.github.com>
2020-12-06 19:11:46 +0530

    Add files via upload

---
+++ HouseKeeping.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HouseKeeping.aspx.cs" Inherits="Lab6Q1.HouseKeeping" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Choose a staff id:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="lstID" runat="server" AutoPostBack="true" OnSelectedIndexChanged="lstID_SelectedIndexChanged">
+            </asp:DropDownList>
+            <br />
+            <asp:Label ID="lblInfo" runat="server"></asp:Label>
+            <br />
+            <asp:ListBox ID="lstCity" runat="server">
+                <asp:ListItem>Buxar</asp:ListItem>
+                <asp:ListItem>Gurugram</asp:ListItem>
+                <asp:ListItem>Udupi</asp:ListItem>
+            </asp:ListBox>
+            <br />
+            <asp:Button ID="btnUpdate" OnClick="btnUpdate_Click" runat="server" Text="Update" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ HouseKeeping.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Lab6Q1
+{
+    public partial class HouseKeeping : System.Web.UI.Page
+    {
+        public string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=HouseKeeping;Integrated Security=SSPI";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!this.IsPostBack)
+            {
+                string selectSQL = "SELECT StaffID FROM Staff";
+                SqlConnection con = new SqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand(selectSQL, con);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataSet dsStaff = new DataSet();
+                try
+                {
+                    con.Open();
+                    adapter.Fill(dsStaff, "Staff");
+                }
+                catch(Exception err)
+                {
+                    lblInfo.Text = "Error in retrieving data. ";
+                    lblInfo.Text += err.Message;
+                }
+                finally
+                {
+                    con.Close();
+                }
+                foreach(DataRow row in dsStaff.Tables["Staff"].Rows)
+                {
+                    ListItem item = new ListItem();
+                    item.Text = row["StaffID"].ToString();
+                    item.Value = row["StaffID"].ToString();
+                    lstID.Items.Add(item);
+                }
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string updateSQL = "Update Staff Set City=@city";
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(updateSQL, con);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataSet dbStaff = new DataSet();
+            cmd.Parameters.AddWithValue("@city", lstCity.SelectedValue);
+            try
+            {
+                con.Open();
+                adapter.Fill(dbStaff, "Staff");
+            }
+            catch (Exception err)
+            {
+                lblInfo.Text = "Error in updating data. ";
+                lblInfo.Text += err.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+            lblInfo.Text = "Row Updated.";
+        }
+
+        protected void lstID_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectSql = "Select * from Staff where StaffID=" + lstID.SelectedValue;
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(selectSql, con);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataSet dbStaff = new DataSet();
+            try
+            {
+                con.Open();
+                adapter.Fill(dbStaff, "Staff");
+            }
+            catch (Exception err)
+            {
+                lblInfo.Text = "Error in retrieving data. ";
+                lblInfo.Text += err.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+            lblInfo.Text = "ID= " + dbStaff.Tables["Staff"].Rows[0]["StaffID"];
+            lblInfo.Text += "<br/>First Name= " + dbStaff.Tables["Staff"].Rows[0]["FirstName"];
+            lblInfo.Text += "<br/>Last Name= " + dbStaff.Tables["Staff"].Rows[0]["LastName"];
+            lblInfo.Text += "<br/>Dno Name= " + dbStaff.Tables["Staff"].Rows[0]["DNo"];
+            lblInfo.Text += "<br/>Street= " + dbStaff.Tables["Staff"].Rows[0]["Street"];
+            lblInfo.Text += "<br/>City= " + dbStaff.Tables["Staff"].Rows[0]["City"];
+            lblInfo.Text += "<br/>State= " + dbStaff.Tables["Staff"].Rows[0]["State"];
+            lblInfo.Text += "<br/>Zipcode= " + dbStaff.Tables["Staff"].Rows[0]["ZipCode"];
+        }
+    }
+}
